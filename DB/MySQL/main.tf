818deca0d44220ee9ece2dcc965e2ba50847bbe8
Rahul S <rshettigar@digitalocean.com>
2024-07-15 16:48:33 +0530

    updated

--- DB/MySQL/main.tf
+++ DB/MySQL/main.tf
@@ -2,8 +2,8 @@ resource "digitalocean_database_cluster" "mysql" {
   name       = "mysql-tf"
   engine     = "mysql"
   version    = "8"
-  size       = "db-s-2vcpu-4gb"
+  size       = "db-intel-2vcpu-4gb"
   region     = "blr1"
   node_count = 3 #creates 1 primary and two standby nodes
-  storage_size_mib = 40960 #slug allocated storage + exact 10Gib
+  #storage_size_mib = 40960 #slug allocated storage + exact 10Gib
 }
